a3ad7faea566cbba93a915cf81350c6bd8b280a7
successdevop <successraphael28@gmail.com>
2023-12-30 20:35:17 +0100

    butit the header, sidebar and main components for proper app layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,30 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import SideBar from "./SideBar";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  min-height: 100dvh;
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <SideBar />
-      <main>
+
+      <Main>
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 }
 
--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,5 +1,27 @@
+import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+import Row from "./Row";
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1/-1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function SideBar() {
-  return <aside>SideBar</aside>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default SideBar;
